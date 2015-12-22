1de11f39b7fbe2a449c76a3fb15bf2d00b135533
Alejandro Zamora <Alejandro Zamora>
2015-12-21 20:27:26 -0500

    agregado bootstrap e interactividad

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :statuses
+  
+  scope "(:locale)", locale: /es|en/ do
+  resources :statuses
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
